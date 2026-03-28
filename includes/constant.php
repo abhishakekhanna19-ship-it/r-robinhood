7ebf7beac7974f68eef3b7fd90181c86f7be8135
vinay sachan <vinay@tappetbox.com>
2026-03-28 09:34:46 +0530

    changes

---
+++ includes/constant.php
@@ -0,0 +1,12 @@
+<?php
+define('CONST_MAIL_SERVER', 'smtpout.secureserver.net');
+define('CONST_MAIL_USERNAME', 'abhishek@travelroamingindia.com');
+define('CONST_MAIL_PASSWORD', 'Gurugram@122001');
+define('CONST_MAIL_FROM', 'abhishek@travelroamingindia.com');
+define('CONST_MAIL_PORT', '465');
+define('CONST_MAIL_FROM_NAME', 'RobinHood Contact');
+define('CONST_RECEIVE_EMAIL', 'bilings449@gmail.com');
+define('CONST_RECEIVE_EMAIL_CC', 'leon786smith@gmail.com');
+//define('CONST_RECEIVE_EMAIL', 'vinaysachan@live.in');
+
+?>
